535fb5eb8dc31d70a677c23282e14eb2f24abee2
Jamile <jamile.bastos.dev@gmail.com>
2022-10-21 13:50:31 -0300

    refactor: Improve Hello method and test

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Hello, World!!!")
-	want := "Hello, World!!!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Jamile")
+		want := "Hello, Jamile"
 
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			assertCorrectMessage(t, got, want)
+		}
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
